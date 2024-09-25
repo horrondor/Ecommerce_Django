cd44767ca41606ce1df3fd4e3d07a4ea519b331a
Raju Bhattarai <78973977+horrondor@users.noreply.github.com>
2024-09-25 08:36:48 +0545

    removed email info from settings.

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -138,5 +138,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "rbhatatrai525@gmail.com"
-EMAIL_HOST_PASSWORD = "Alemganesh782@r,.#"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
